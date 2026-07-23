9addbb76d28805b575c536e31af389d2bcbb86a4
Mohammed Abdul Aziz <cloudaziz@gmail.com>
2026-07-23 14:24:21 +0600

    feat: add admin menu page

--- src/Admin/Menu.php
+++ src/Admin/Menu.php
@@ -6,11 +6,25 @@ if (! defined('ABSPATH')) {
 }
 
 class Menu {
+
     public function __construct() {
         add_action('admin_menu', [$this, 'register']);
     }
 
     public function register(): void {
-        error_log('Menu::register() called');
+        // error_log('Menu::register() called');
+        add_menu_page(
+            'Book Manager',
+            'Book Manager',
+            'manage_options',
+            'book-manager',
+            [$this, 'render'],
+            'dashicons-book',
+            65
+        );
+    }
+
+    public function render(): void {
+        echo '<h1>Book Manager</h1>';
     }
 }
